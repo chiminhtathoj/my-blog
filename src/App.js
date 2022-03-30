5ab146cccb52bda604cb483e7ad4e041c9e36cf4
chiminhtathoj <vdlong2009@gmail.com>
2022-03-30 21:27:18 +0700

    create a new post component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import TopBar from "./components/Topbar/Topbar"
 import Home from "./Pages/Home/Home";
+import Write from "./Pages/Write/Write";
 function App() {
   return (
     <>
       <TopBar />
-      <Home />
+      <Write/>
     </>
   );
 }
